909bb6fa12acde2f2afe08885ff7409526f5660c
Sage Jasinski <sagejasinski@gmail.com>
2022-07-29 17:51:04 -0700

    Nav bar creation

---
+++ README.txt
@@ -0,0 +1,4 @@
+This site was made from scratch by Sage Jasinski
+You can find me on github and most social platforms
+
+w3schools was instrumental in helping figure out what the hell i was doing
---
+++ app.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "200px";
+    document.getElementsByClassName("main").style.marginLeft = "200px";
+    document.getElementsByClassName("display-img").style.marginLeft= "200px"
+}
+
+function closeNav(){
+    document.getElementById("mySidebar").style.width = "0px";
+    document.getElementsByClassName("main").style.marginLeft = "0px";
+    document.getElementsByClassName("display-img").style.marginLeft= "0px"
+
+}
