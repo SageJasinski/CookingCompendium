40533b16436b0aafbd486187a649c1b6dd709351
Sage Jasinski <sagejasinski@gmail.com>
2023-01-09 23:01:54 +0000

    start of react reconfiguration

--- cookingcompendium/src/App.js
+++ cookingcompendium/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import './Styles/App.scss';
+import React from 'react';
+import Logo from './images/logo.png'
+import { Navbar, Nav, Container} from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    data: []
+  }
+
+  componentDidMount() {
+    const data = require('./Database.json');
+    this.setState({data})
+  }
+
+
+  render(){
+    console.log(this.state.data);
+    return(
+      <>
+
+      <div className='Head'>
+        <img className='logo' src={Logo}  alt='Logo of the company'/>
+        <h1>Cooking Compendium</h1>
+
+        <Navbar className='main-nav'>
+          <Container>
+            <Nav>
+              <Nav.Link to="/" className='nav-link'>Meal</Nav.Link>
+              <Nav.Link to="/" className='nav-link'>Dessert</Nav.Link>
+              <Nav.Link to="/" className='nav-link'>A-Z</Nav.Link>
+              <Nav.Link to="/" className='nav-link'>Holiday</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </div>
+
+        <div className='scroll-box'>
+          {this.state.data.map(item => (
+              <Card key={item.id}>
+                <Card.Img src={item.image} />
+                <Card.Body>
+                <Card.Title>{item.title}</Card.Title>
+                </Card.Body>
+              </Card>
+          ))}
+        </div>
+      </>
+    )
+  }
 }
 
 export default App;
