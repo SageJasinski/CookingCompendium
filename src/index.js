c0742cc4791daa0e7fe8f32ba1a631986db83509
Sage Jasinski <sagejasinski@gmail.com>
2023-01-17 10:52:01 +0000

    routes added for diffrent pages

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import Recipe from './Recipe';
 import Footer from './Footer';
 import './Styles/Index.scss';
 
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route exact path='/' element={
+      <div className='flow'>
+        <App />
+        <Footer/>
+     </div>}/>
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <div className='flow'>
-      <App />
-      <Footer/>
-     </div>
-  </React.StrictMode>
-);
+      <Route path='/recipe' element={<Recipe/>}/>
+    </Routes>
+  </BrowserRouter>
+)
